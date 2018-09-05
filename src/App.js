31d1d854d1e3d12dfa23afb4cb00a909b51b1982
= <=>
2018-09-05 22:43:27 +0300

    added quote's api endpoint

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import './App.css';
 class App extends Component {
 
   componentDidMount(){
-    fetch("https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").then(function(response){
+
+    /**
+     * Url for the wordpress's quote api, it returns a random quote
+     */
+    let quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+    fetch(quoteUrl).then(function(response){
       if(response.ok){
         response.json().then(function(json) {
-          let quotes = json;
-          console.log(quotes);
+          let quote = json;
+          console.log(quote);
         });
       }
     })
