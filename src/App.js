c2cd9631bacd6f74764a10cfe73473b37f578214
= <=>
2018-09-05 23:09:32 +0300

    added the constructor and setted the state of quote variable

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import QuoteGenerator from './quoteGenerator';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
 
-  componentDidMount(){
-
+  constructor(props) {
+    super(props);
     this.state = {
       quote: ''
-    }
+    };
+  }
 
+  componentDidMount(){
+    let that = this;
     /**
      * Url for the wordpress's quote api, it returns a random quote
      */
     let quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+    // Fetching the requested quote
     fetch(quoteUrl).then(function(response){
       if(response.ok){
+        // Converting response to json
         response.json().then(function(json) {
-          let quote = json;
-          console.log(quote);
+          // Updating state of quote
+          that.setState({
+            quote: json
+          });
+          console.log(that.state.quote)
         });
       }
     })
