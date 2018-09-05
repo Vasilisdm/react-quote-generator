cc0997ae82291ded675e43a5bf74b7828c0efb4b
= <=>
2018-09-05 08:10:45 +0300

    added a heading

--- src/quoteGenerator.js
+++ src/quoteGenerator.js
@@ -4,6 +4,7 @@ class QuoteGenerator extends Component{
     render(){
         return(
             <section className="quote-generator-container">
+                <h1 className="quote-generator-heading">Quote Generator</h1>
                 <div id="text"></div>
                 <div id="author"></div>
                 <button id="new-quote"></button>
