997f5da234fbbe93c01e630e6ca39a3700917c31
= <=>
2018-09-06 09:25:56 +0300

    renamed the component

---
+++ src/components/QuoteGeneratorBtn.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class QuoteGeneratorBtn extends Component {
+    render(){
+        return(
+            <button id="new-quote">New Quote</button>
+        )
+    }
+}
+
+export default QuoteGeneratorBtn;
