2796935e60f0b119d5374992dbc3e51392b06988
= <=>
2018-09-06 09:12:20 +0300

    Renamed the quoteGenerator to quoteContainer

---
+++ src/components/quoteContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class QuoteGenerator extends Component{
+    render(){
+        return(
+            <section className="quote-generator-container">
+                <h1 className="quote-generator-heading">Quote Generator</h1>
+                {/* Used replace to remove any unnecessary html tags */}
+                <div id="text">{this.props.content.replace(/(<([^>]+)>)/ig,"")}</div>
+                <div id="author">{this.props.author}</div>
+                <button id="new-quote">New Quote</button>
+                <a id="tweet-quote"></a>
+            </section>
+        )
+    }
+}
+
+export default QuoteGenerator;
