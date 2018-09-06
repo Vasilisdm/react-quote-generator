4e419dd03e7e57b18906a6bb1719b6d72d350ea7
= <=>
2018-09-06 09:33:16 +0300

    passed on click event to child component

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
     return (
       <div id="App">
         <QuoteContainer content={this.state.quote} author={this.state.author}/>
-        <QuoteGeneratorBtn />
+        <QuoteGeneratorBtn generateQuote={this.getRandomQuote}/>
       </div>
     );
   }
