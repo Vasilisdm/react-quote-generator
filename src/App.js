19c18472242cee054d7a25a7e720be786fbb9107
= <=>
2018-09-06 08:56:36 +0300

    changed the import statement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import QuoteGenerator from './quoteGenerator';
+import QuoteGenerator from './components/quoteGenerator';
 import './App.css';
 
 class App extends Component {
