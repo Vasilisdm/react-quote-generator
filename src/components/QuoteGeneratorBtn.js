1cb08e8b71611e8c840fa2410c941517e67974c9
= <=>
2018-09-06 09:32:53 +0300

    added onClick event listener

--- src/components/QuoteGeneratorBtn.js
+++ src/components/QuoteGeneratorBtn.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class QuoteGeneratorBtn extends Component {
     render(){
         return(
-            <button id="new-quote">New Quote</button>
+            <button id="new-quote" onClick={this.props.generateQuote}>New Quote</button>
         )
     }
 }
