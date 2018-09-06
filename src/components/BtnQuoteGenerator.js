5a76a8119571489a273c1f27fbf92594b8cf6ccb
= <=>
2018-09-06 09:22:16 +0300

    created quote button generator component

---
+++ src/components/BtnQuoteGenerator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class QuoteGenerator extends Component {
+    render(){
+        return(
+            <button id="new-quote">New Quote</button>
+        )
+    }
+}
+
+export default QuoteGenerator;
