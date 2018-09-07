d1a0a0d3f1d8bb317656c4b44461750b77edd848
= <=>
2018-09-07 07:55:57 +0300

    assigned content and author to this.props

--- src/components/quoteContainer.js
+++ src/components/quoteContainer.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class QuoteContainer extends Component{
     render(){
+        let { content, author } = this.props;
         return(
             <section className="quote-generator-container">
                 <h1 className="quote-generator-heading">Quote Generator</h1>
                 {/* Used replace to remove any unnecessary html tags */}
-                <div id="text">{this.props.content.replace(/(<([^>]+)>)/ig,"")}</div>
-                <div id="author">{this.props.author}</div>
+                <div id="text">{content.replace(/(<([^>]+)>)/ig,"")}</div>
+                <div id="author">{author}</div>
                 
                 <a id="tweet-quote"></a>
             </section>
