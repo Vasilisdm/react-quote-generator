2b523efb78f46089ea6ea47e93e659cf7c4b880d
= <=>
2018-09-07 08:24:27 +0300

    created the tweet button component

---
+++ src/components/QuoteTweetBtn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuoteTweetBtn extends Component{
+    render(){
+        return(
+            <section className="tweet-btn-container">
+                <a id="tweet-quote" href="#">Tweet</a>
+            </section>
+        )
+    }
+}
+
+export default QuoteTweetBtn;
