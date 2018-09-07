de46aa9e14f5943006a81f0036146345c1534ab6
= <=>
2018-09-07 08:13:10 +0300

    added the decodeHtml() to remove html entities from quote content/author

--- src/components/quoteContainer.js
+++ src/components/quoteContainer.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
 class QuoteContainer extends Component{
+
+    // Method to remove any html encoded entities
+    decodeHtml(html) {
+        var txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+    }
+
     render(){
         let { content, author } = this.props;
+        console.log(this.decodeHtml(this.html))
         return(
             <section className="quote-generator-container">
                 <h1 className="quote-generator-heading">Quote Generator</h1>
                 {/* Used replace to remove any unnecessary html tags */}
-                <div id="text">{content.replace(/(<([^>]+)>)/ig,"")}</div>
-                <div id="author">{author}</div>
+                <div id="text">{this.decodeHtml(content.replace(/(<([^>]+)>)/ig,""))}</div>
+                <div id="author">{this.decodeHtml(author)}</div>
                 
                 <a id="tweet-quote"></a>
             </section>
